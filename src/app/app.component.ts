1c46c95ca06bd49b2e54041003b771217237ab2a
cstrick-WE <cstrickland@wilburellis.com>
2023-01-13 12:22:06 -0600

    Create pokemon project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+} from '@angular/core';
+import { Store } from '@ngrx/store';
+import { fetchAllPokemon } from './store/app.actions';
+import { selectPokemon } from './store/app.selectors';
+import { AppState, Pokemon } from './store/app.state';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pokemon-webapp';
+  pokemon: Pokemon[];
+  constructor(
+    private store: Store<AppState>,
+    private changeDetection: ChangeDetectorRef
+  ) {}
+
+  ngOnInit(): void {
+    this.store.dispatch(fetchAllPokemon());
+    this.store.select(selectPokemon).subscribe((pokemon) => {
+      this.pokemon = pokemon;
+      this.changeDetection.detectChanges();
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PokemonModule } from './feature/pokemon/pokemon.module';
+import { AppEffects } from './store/app.effects';
+import { appReducer } from './store/app.reducer';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({ app: appReducer }),
+    EffectsModule.forRoot([AppEffects]),
+    HttpClientModule,
+    PokemonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
